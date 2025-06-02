1d6775a49e9e544f0f05bfe4820e1176228d3583
MSP_31 <jhss9747@gmail.com>
2025-06-02 15:13:21 +0900

    feat: "클레임 페이지 구현중"

--- homs-fe/src/pages/user/order/OrderItemList.vue
+++ homs-fe/src/pages/user/order/OrderItemList.vue
@@ -91,13 +91,15 @@
             </DynamicTable>
 
         <!-- 페이지 네비 -->
-        <PageNav :currentPage="Number(currentPage)" :totalPages="Number(totalPages)" @set-page="handleSetPage"> </PageNav>
+        <PageNav :currentPage="Number(currentPage)" :totalPages="Number(totalPages)" @set-page="handleSetPage">
+        </PageNav>
         <!-- 제품 상세 모달 -->
         <ProductDetail :visible="showModal" :productId="Number(selectedId)" @close="showModal = false"></ProductDetail>
         <!-- 신청 모달 -->
         <OrderRequestModal :visible="showReqeustModal" :text="modalText" @update:visible="showReqeustModal = $event" @confirm="orderConfirm" @cancel="showReqeustModal = false" :order-id="orderId"  />
         <!-- 클레임 모달 -->
-        <ClaimRequestModal :visible="showClaimModal" :text="modalText" @update:visible="showClaimModal = $event" @confirm="claimConfirm" @cancel="showClaimModal = false" />
+        <ClaimRequestModal :visible="showClaimModal" :text="modalText" @update:visible="showClaimModal = $event"
+            @confirm="claimConfirm" @cancel="showClaimModal = false" />
     </div>
 </template>
 
