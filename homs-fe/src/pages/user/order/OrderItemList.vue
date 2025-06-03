b876556107d85349ca050b720b7702f165cc7b38
MSP_31 <jhss9747@gmail.com>
2025-06-03 15:34:27 +0900

    fix: "발주 신청 오류 수정"

--- homs-fe/src/pages/user/order/OrderItemList.vue
+++ homs-fe/src/pages/user/order/OrderItemList.vue
@@ -63,7 +63,7 @@
                 <div v-if="item && item.productQuantity === null">데이터 없음</div>
                 <div v-else-if="item && item.productQuantity !== undefined && !item.isEditing">{{
                     item.productQuantity
-                }}</div>
+                    }}</div>
                 <div v-else-if="item && item.productQuantity !== undefined && item.isEditing">
                     <input type="number"
                         class="rounded mr-2 border-1 border-gray-300 w-15 focus:border-orange-500 focus:outline-none"
@@ -146,7 +146,7 @@ const currentActionType = ref("");
 // 알람 모달 관련
 const showConfirmModal = ref(false); // 알람 모달 상태
 const modalType = ref(""); // 알람 모달 식별 타입
-const alert = ref(false); // 알람 모달 여부
+const alertModal = ref(false); // 알람 모달 여부
 
 const currentPage = ref(1); // 현재 페이지 상태 관리
 const totalPages = ref(0); // 총 페이지 수 상태 관리
@@ -252,7 +252,7 @@ const editBtn = async (item) => {
         if (item.isEditing === false) {
             await apiClient.put(`/orderitem/${orders.value.orderId}/renew/${item.productId}?quantity=${item.productQuantity}`);
             
-            alert.value = true;
+            alertModal.value = true;
             showConfirmModal.value = true;
             modalText.value = "수정되었습니다.";
         }
@@ -262,7 +262,7 @@ const editBtn = async (item) => {
 // 주문 단일 취소
 const deleteBtn = (productId) => {
     selectedId.value = productId;
-    alert.value = false;
+    alertModal.value = false;
     showConfirmModal.value = true;
     modalText.value = t("script.delete");
     modalType.value = "orderCancle";
@@ -271,11 +271,11 @@ const deleteBtn = (productId) => {
 // 주문 일괄 취소
 const deleteItems = async (selectedItems) => {
     if (selectedItems.value.length <= 0) {
-        alert.value = true;
+        alertModal.value = true;
         showConfirmModal.value = true;
         modalText.value = "항목을 선택해주세요!";
     } else {
-        alert.value = false;
+        alertModal.value = false;
         showConfirmModal.value = true;
         modalText.value = selectedUserIds.value.length + "개의 항목을 정말로 삭제하시겠습니까?";
         modalType.value = "orderCancles";   
@@ -292,12 +292,12 @@ const deletePostData = async (params) => {
                 return params.productIds.map((id) => `productIds=${id}`).join("&"); // 배열을 올바르게 직렬화
             },
         });
-        alert.value = true;
+        alertModal.value = true;
         showConfirmModal.value = true;
         modalText.value = "해당 상품의 주문이 취소되었습니다.";
         fetchData();
     } catch (error) {
-        alert.value = true;
+        alertModal.value = true;
         showConfirmModal.value = true;
         modalText.value = error;
     }
